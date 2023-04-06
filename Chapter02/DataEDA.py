3cd28ed9c1da54fc61bb07e2fd2fa29664a9d6f7
Thinktan <tantao0675@hnu.edu.cn>
2023-04-07 01:08:14 +0800

    up

---
+++ Chapter02/DataEDA.py
@@ -0,0 +1,9 @@
+# import packages we need for exploratory data analysis (EDA)
+import pandas as pd  # to store tabular data
+import numpy as np  # to do some math
+import matplotlib.pyplot as plt  # a popular data visualization tool
+import seaborn as sns  # another popular data visualization tool
+
+plt.style.use('fivethirtyeight')  # a popular data visualization theme
+
+
